6b5ee01b255041631383c3a9db4003f087615c34
Bryce Eadie <bryce.eadie@gmail.com>
2021-08-08 10:03:26 -0700

    Adds validation for matching passwords and unique usernames

--- ecr/views.py
+++ ecr/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def home(request):
     return render(request, 'ecr/home.html')
@@ -11,9 +12,11 @@ def signupuser(request):
         return render(request, 'ecr/signupuser.html', {'form':UserCreationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+            except IntegrityError:
+                return render(request, 'ecr/signupuser.html', {'form':UserCreationForm(), 'error':'That username is already taken.  Please make another selection.'})
         else:
-            pass
-            #return error
+            return render(request, 'ecr/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
 
